df26c9fa73c40d810b29b52b2bdce4de4c743654
Archietheowl <adsg1206@gmail.com>
2025-06-24 15:06:15 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This is the fix
-  server: {
-    host: 'localhost'
-  }
+  base: '/',
 })
